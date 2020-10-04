06b59c633a1f751b7327601cd7856e9f8297f55e
Muhammad Shalahuddin Yahya Sunarko <muhammadsyahyas@gmail.com>
2020-10-04 13:29:15 +0700

    More info on test

--- test/request_template_matching.py
+++ test/request_template_matching.py
@@ -1,3 +1,4 @@
+import time
 from pprint import pprint
 
 import requests
@@ -16,9 +17,16 @@ def req_template_matching():
         "projectName": "test_project",
         "userId": "test_user"
     }
+    t_s = time.perf_counter()
     resp = requests.post(
         "http://172.17.0.1:3080/api/template_matching",
         json=post_body
     )
+    t_e = time.perf_counter()
+    print("Processing time: %.2f seconds" % (t_e - t_s))
     print("Status code:", resp.status_code)
     pprint(resp.content)
+
+
+if __name__ == "__main__":
+    req_template_matching()
