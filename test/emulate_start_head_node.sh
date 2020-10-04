db2f1c1146506fb3bd85c20257a0429b6d3d9370
Muhammad Shalahuddin Yahya Sunarko <muhammadsyahyas@gmail.com>
2020-10-04 12:59:40 +0700

    Added testing

---
+++ test/emulate_start_head_node.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+docker run \
+--rm -it \
+--network=host \
+--env-file=defaults.env \
+--env-file=config.env \
+--ulimit nofile=65536:65536 \
+--name="autolabel-model-service-head-node" \
+--shm-size=512M \
+--volume=${PWD}/autolabel_models:/home/gawai/autolabel_models \
+--volume=${PWD}/ray_node_scripts:/home/gawai/ray_node_scripts \
+--volume=/home/yahya:/home/yahya:ro \
+--workdir=/home/gawai \
+--memory=4G \
+--memory-swap=4G \
+--cpus=2 \
+gaw-ai/autolabel-models-base:latest \
+ray_node_scripts/./ray_start_head.sh
---
+++ test/request_template_matching.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+
+import requests
+
+
+def req_template_matching():
+    post_body = {
+        "templates": [
+            "file:///home/yahya/Documents/gawai/template_matching_dev/autolabel_model/template/solenoid1.jpg",
+            "file:///home/yahya/Documents/gawai/template_matching_dev/autolabel_model/template/angle_valve.jpg"
+        ],
+        "images": [
+            "file:///home/yahya/Documents/gawai/template_matching_dev/autolabel_model/sample_1000dpi/test-1.jpg",
+            "file:///home/yahya/Documents/gawai/template_matching_dev/autolabel_model/sample_1000dpi/test3-2.jpg"
+        ],
+        "projectName": "test_project",
+        "userId": "test_user"
+    }
+    resp = requests.post(
+        "http://172.17.0.1:3080/api/template_matching",
+        json=post_body
+    )
+    print("Status code:", resp.status_code)
+    pprint(resp.content)
