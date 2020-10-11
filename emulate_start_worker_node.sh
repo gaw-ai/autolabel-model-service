5a9cadf2a7bc41aef63cbb5bb3b49ba87b6a8eee
Muhammad Shalahuddin Yahya Sunarko <muhammadsyahyas@gmail.com>
2020-10-11 09:57:02 +0700

    Emulate

--- emulate_start_worker_node.sh
+++ emulate_start_worker_node.sh
@@ -5,14 +5,14 @@ docker run \
 --env-file=defaults.env \
 --env-file=config.env \
 --ulimit nofile=65536:65536 \
---name="autolabel-model-service-head-node" \
---shm-size=512M \
+--name="autolabel-model-service-worker-node" \
+--shm-size=384M \
 --volume=${PWD}/autolabel_models:/home/gawai/autolabel_models \
 --volume=${PWD}/ray_node_scripts:/home/gawai/ray_node_scripts \
 --volume=/home/yahya:/home/yahya:ro \
 --workdir=/home/gawai \
---memory=2G \
---memory-swap=2G \
---cpus=1 \
+--memory=4G \
+--memory-swap=4G \
+--cpus=2 \
 gaw-ai/autolabel-models-base:latest \
-ray_node_scripts/./ray_start_head.sh
+ray_node_scripts/./ray_start_worker.sh
